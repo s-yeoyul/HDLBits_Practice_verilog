61243b6df13b5bd560c1dab2489c82d7cd81b416
Sangmyeon Yoon <81800659+s-yeoyul@users.noreply.github.com>
2025-02-16 18:18:35 +0900

    Update 119. Simple FSM 1 (synchronous reset).v
    modify: simplify synchronous reset logic

--- 119. Simple FSM 1 (synchronous reset).v
+++ 119. Simple FSM 1 (synchronous reset).v
@@ -8,45 +8,23 @@ module top_module(clk, reset, in, out);
 
     // Fill in state name declarations
 
-    reg [1:0] present_state, next_state;
-	parameter state_b = 2'b01, state_a = 2'b10;
-    
-    always @ (*)
-        begin
-           present_state = next_state; 
+    reg present_state, next_state;
+
+    always @(posedge clk) begin
+        if (reset) begin  
+            // Fill in reset logic
+        end else begin
+            case (present_state)
+                // Fill in state transition logic
+            endcase
+
+            // State flip-flops
+            present_state = next_state;   
+
+            case (present_state)
+                // Fill in output logic
+            endcase
         end
-    
-    always @ (posedge clk)
-        begin
-            if(reset)
-                next_state <= state_b[1:0];
-           	else
-                begin
-                    case(present_state[1:0])
-                        state_b : begin
-                            if(~in)
-                                next_state <= state_a[1:0];
-                           	else
-                                next_state <= state_b[1:0];
-                        end
-                        
-                        state_a : begin
-                            if(~in)
-                                next_state <= state_b[1:0];
-                            else
-                                next_state <= state_a[1:0];
-                        end
-                        
-                        
-                    endcase
-                end
-        end
-    
-    assign out = (present_state == state_b[1:0]);
+    end
 
 endmodule
-
-
-//But in this code, its doesn't take advantage of the recommended scheme.
-//So if you have a better way to solve this problem, please pull a request so I can merge it.
-//THX
